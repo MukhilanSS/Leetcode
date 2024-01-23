3a5a243db306cf713cdd4c029b6d082410cba71e
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-01-24 00:37:00 +0530

    Time: 2 ms (93.69%), Space: 44.3 MB (70.65%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,21 +1,28 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int n=s.length();
-        int a[]=new int[26];
-        for(int i=0;i<n;i++)
-        a[s.charAt(i)-'a']++;
-        for(int i=0;i<n;i++){
-            if(a[s.charAt(i)-'a']==1)
-            return i;
-        }
-        
-        // Map<Character,Integer>a=new HashMap<>();
-        // for(char c:s.toCharArray())
-        // a.put(c,a.getOrDefault(c,0)+1);
+        // int n=s.length();
+        // int a[]=new int[26];
+        // for(int i=0;i<n;i++)
+        // a[s.charAt(i)-'a']++;
         // for(int i=0;i<n;i++){
-        //     if(a.get(s.charAt(i))==1)
+        //     if(a[s.charAt(i)-'a']==1)
         //     return i;
         // }
-        return -1;
+        
+        // // // Map<Character,Integer>a=new HashMap<>();
+        // // // for(char c:s.toCharArray())
+        // // // a.put(c,a.getOrDefault(c,0)+1);
+        // // // for(int i=0;i<n;i++){
+        // // //     if(a.get(s.charAt(i))==1)
+        // // //     return i;
+        // // // }
+        // return -1;
+        int a=Integer.MAX_VALUE;
+        for(char c='a';c<='z';c++){
+            int index=s.indexOf(c);
+            if(index!=-1 && index==s.lastIndexOf(c))
+            a= Math.min(index,a);
+        }
+        return a==Integer.MAX_VALUE?-1:a;
     }
 }
