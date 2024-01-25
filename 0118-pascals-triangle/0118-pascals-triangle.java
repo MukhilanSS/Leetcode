5aa3cb1b05134fd4419ca20e706ef5e10f6138f8
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-01-26 01:46:54 +0530

    Time: 1 ms (90.32%), Space: 41.9 MB (44.08%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,17 +1,33 @@
 class Solution {
     public List<List<Integer>> generate(int n) {
-        List<List<Integer>> a=new ArrayList<>();
-        int k ;
-        for(int i=1;i<=n;i++)
-        {
-            k=1;
-            List<Integer>b=new ArrayList<>();
-            for(int j=1;j<=i;j++){
-                b.add(k);
-                k=k*(i-j)/j;
+        // List<List<Integer>> a=new ArrayList<>();
+        // int k ;
+        // for(int i=1;i<=n;i++)
+        // {
+        //     k=1;
+        //     List<Integer>b=new ArrayList<>();
+        //     for(int j=1;j<=i;j++){
+        //         b.add(k);
+        //         k=k*(i-j)/j;
+        //     }
+        //     a.add(b);
+        // }
+        // return a;
+        List<List<Integer>> ans=new ArrayList<>(Arrays.asList(
+            Arrays.asList(1),Arrays.asList(1,1)));
+        if(n==1)
+        ans.remove(1);
+        for(int i=2;i<n;i++){
+            List<Integer> a=ans.get(i-1);
+            List<Integer> b=new ArrayList<>();
+            b.add(1);
+            for(int j=0;j<a.size()-1;j++)
+            {   
+                b.add(a.get(j)+a.get(j+1));
             }
-            a.add(b);
+            b.add(1);
+            ans.add(b);
         }
-        return a;
+        return ans;
     }
 }
