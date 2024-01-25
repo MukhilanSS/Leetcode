594af7972d758f6996a0340f53a350686250fcd2
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-01-25 13:03:50 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (14.30%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -5,17 +5,18 @@ class Solution {
         int col=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
+                if(matrix[i][j]==0)
+                {
                     matrix[i][0]=0;
                     if(j!=0)
                     matrix[0][j]=0;
                     else
                     col=0;
                 }
+
             }
         }
-        for(int i=1;i<m;i++)
-        {
+        for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 if(matrix[i][j]!=0){
                     if(matrix[0][j]==0 || matrix[i][0]==0)
