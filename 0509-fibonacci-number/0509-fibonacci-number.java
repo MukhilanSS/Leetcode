00196107bb2e7cdbd56ffc3b2c512d7da87d3d5e
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-02-05 22:37:07 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (70.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,10 @@
 class Solution {
     public int fib(int n) {
         if(n==0) return 0;
-        else if(n==1 || n==2) return 1;
-        return fib(n-1)+fib(n-2);
+        int a[]=new int[n+1];
+        a[1]=1;
+        for(int i=2;i<=n;i++)
+        a[i]=a[i-1]+a[i-2];
+        return a[n];
     }
 }
