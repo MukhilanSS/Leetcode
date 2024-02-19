c64ad9516c076f2b2204a3be312721943e0232ac
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-02-19 19:09:09 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (56.93%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -3,15 +3,18 @@ class Solution {
         // System.out.println(Math.log10(n));
         // System.out.println(Math.log10(2));
         // System.out.print(Math.log10(n)/Math.log10(2));
-        if(n==1)
-            return true;
-        else if(n%2!=0 || n==0)
-            return false;   
-        n/=2;
-        return isPowerOfTwo(n);
+        // if(n==1)
+        //     return true;
+        // else if(n%2!=0 || n==0)
+        //     return false;   
+        // n/=2;
+        // return isPowerOfTwo(n);
         // if((n&(n-1))==0 && n>0)
         // return true;
         // return false;
         // return (Math.log10(n)/Math.log10(2)%1==0);
+        if(n<=0)
+            return false;
+        return ((n&(n-1))==0);
     }
 }
