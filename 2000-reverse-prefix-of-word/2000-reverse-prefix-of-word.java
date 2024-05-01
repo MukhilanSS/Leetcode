4e045b5831633b519186a14a4df69e822129cfc2
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-05-02 00:49:27 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (56.56%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -12,3 +12,14 @@ class Solution {
         return s.toString();
     }
 }
+// class Solution {
+//     public String reversePrefix(String word, char ch) {
+//         int firstOccurence = word.indexOf(ch);
+//         if(firstOccurence == -1){
+//             return word;
+//         }
+        
+//         StringBuilder prefix = new StringBuilder(word.substring(0, firstOccurence+1));
+//         return prefix.reverse().toString() + word.substring(firstOccurence+1);
+//     }
+// }
