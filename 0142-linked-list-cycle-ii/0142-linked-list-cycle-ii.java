dee84fc086c809bac2207d77d0d8436953015e4a
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-07-07 23:53:51 +0530

    Time: 1 ms (28.21%), Space: 43.2 MB (99.89%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -13,31 +13,47 @@ public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode fast=head;
         ListNode slow=head;
-        int length=0;
         while(fast!=null && fast.next!=null){
             fast=fast.next.next;
             slow=slow.next;
-            if(fast==slow){
-                ListNode temp=slow;
-                do{
-                    temp=temp.next;
-                    length++;
-                }while(temp!=slow);
-                break;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast)
+                {
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                System.gc();
+                return slow;
             }
         }
-        ListNode f=head;
-        ListNode s=head;
-        if(length==0) return null;
-        while(length>0){
-            s=s.next;
-            length--;
-        }
-        while(f!=s)
-        {
-            s=s.next;
-            f=f.next;
-        }
-        return s;
+        System.gc();
+        return null;
+        // int length=0;
+        // while(fast!=null && fast.next!=null){
+        //     fast=fast.next.next;
+        //     slow=slow.next;
+        //     if(fast==slow){
+        //         ListNode temp=slow;
+        //         do{
+        //             temp=temp.next;
+        //             length++;
+        //         }while(temp!=slow);
+        //         break;
+        //     }
+        // }
+        // ListNode f=head;
+        // ListNode s=head;
+        // if(length==0) return null;
+        // while(length>0){
+        //     s=s.next;
+        //     length--;
+        // }
+        // while(f!=s)
+        // {
+        //     s=s.next;
+        //     f=f.next;
+        // }
+        // return s;
     }
 }
