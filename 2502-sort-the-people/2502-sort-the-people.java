7be5043e686a98e44310f7f78401130104f15ce7
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-07-22 15:13:35 +0530

    Time: 7 ms (90.64%), Space: 45.2 MB (67.94%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -8,18 +8,28 @@ class Solution {
         }
     }
     public String[] sortPeople(String[] names, int[] heights) {
-        List<Pair>list=new ArrayList<>();
-        int n=names.length;
-        for(int i=0;i<n;i++)
-        {
-            list.add(new Pair(names[i],heights[i]));
-        }
-        list.sort((n1,n2)->n2.height-n1.height);
-        String ans[]=new String[n];
+        // List<Pair>list=new ArrayList<>();
+        // int n=names.length;
+        // for(int i=0;i<n;i++)
+        // {
+        //     list.add(new Pair(names[i],heights[i]));
+        // }
+        // list.sort((n1,n2)->n2.height-n1.height);
+        // String ans[]=new String[n];
         
-        for(int i=0;i<n;i++)
+        // for(int i=0;i<n;i++)
+        // {
+        //     ans[i]=list.get(i).name;
+        // }
+        // return ans;
+        PriorityQueue<Integer>heap=new PriorityQueue<>((n1,n2)->heights[n2]-heights[n1]);
+        for(int i=0;i<heights.length;i++)
+            heap.offer(i);
+        String ans[]=new String[names.length];
+        int k=0;
+        while(!heap.isEmpty())
         {
-            ans[i]=list.get(i).name;
+            ans[k++]=names[heap.poll()];
         }
         return ans;
     }
