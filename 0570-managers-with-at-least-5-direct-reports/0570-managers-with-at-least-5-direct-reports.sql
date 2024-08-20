b54028ba159d26fb6b31283e6038b76c7f3fa79d
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-08-20 23:32:42 +0530

    Time: 326 ms (70.24%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 Select a.name from employee a join 
-(Select managerId from employee group by managerId having count(*)>=5) b 
+(Select managerId from employee where managerId is not null group by managerId having count(*)>=5) b 
 on a.id=b.managerId;
