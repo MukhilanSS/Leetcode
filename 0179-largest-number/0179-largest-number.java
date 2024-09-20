244ab7054de626cd24ab3c9168909bb85f7bf04b
Mukhil <117979054+MukhilanSS@users.noreply.github.com>
2024-09-20 19:06:09 +0530

    Time: 5 ms (97.03%), Space: 43 MB (79.22%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -3,8 +3,9 @@ class Solution {
         StringBuilder s=new StringBuilder();
         String temp[]=new String[nums.length];
         for(int i=0;i<nums.length;i++){
-            temp[i]=Integer.toString(nums[i]);
+            temp[i]=""+(nums[i]);
         } 
+
         Arrays.sort(temp,(a,b)->(b+a).compareTo(a+b));
         if(temp[0].equals("0")) return "0";
         for(String str:temp)
